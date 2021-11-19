4339bf215b80bade5e52a2c9339cbfd7970b3ec5
hojunkim1 <76956067+hojunkim1@users.noreply.github.com>
2021-11-19 22:29:27 +0900

    react todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
+  return (
+    <div>
+      <h1>My Todos ({todos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
